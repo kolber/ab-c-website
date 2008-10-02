f0d69ceead419e3adae6cf8f672951c50209eee5
Ben Schwarz <ben.schwarz@gmail.com>
2008-10-03 06:53:03 +1000

    Added config/deploy.rb to deployment to newstreet's very own Capfile for cap env

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+set :application, "ab-c"
+set :repository,  "git@github.com:kolber/ab-c-website.git"
+set :scm, "git"
+set :branch, "master"
+
+set :deploy_via, :copy
+set :deploy_to, "/home/ben/#{application}"
+
+role :app, "ab-c.com.au"
+role :web, "ab-c.com.au"
+role :db,  "ab-c.com.au", :primary => true
+
+# Override tasks not needed for deployment of static files
+namespace :deploy do
+  task :finalize_update do
+    logger.info 'do nothing - overridden finalize_update'
+  end
+
+  task :restart do
+    logger.info 'do nothing - overridden finalize_update'
+  end
+end
